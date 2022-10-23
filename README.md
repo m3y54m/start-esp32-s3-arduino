1d2948d3a68ae7e281246cedf7757fa21fe6b246
Meysam Parvizi <meysam.parvizi.ir@gmail.com>
2022-10-23 03:30:15 +0330

    Blinky example programmed using USB

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Getting started with ESP32-S3 + Arduino
 
 
+## Issues
+
+### `fatal error: pins_arduino.h: No such file or directory`
+
+Set the `"variant"` key in `esp32-s3-wroom-1-n4r2.json` board definition file to `"esp32s3"` defined in `arduino-esp32` repo:
+
+```json
+"variant": "esp32s3"
+```
+
+https://github.com/espressif/arduino-esp32/tree/master/variants
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <Arduino.h>
 
+// the setup function runs once when you press reset or power the board
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(39, OUTPUT);
 }
 
+// the loop function runs over and over again forever
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(39, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(39, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
 }
